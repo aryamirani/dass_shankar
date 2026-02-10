4a634f354659df194240056bba0b445e5b4f7f5e
Arya Mirani <aryamirani06@gmail.com>
2026-02-10 11:51:46 +0530

    fixed the incorrect vite react compile error

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      jsxRuntime: 'classic'
+    })
+  ],
   publicDir: 'public',
+  resolve: {
+    dedupe: ['react', 'react-dom']
+  }
 })
